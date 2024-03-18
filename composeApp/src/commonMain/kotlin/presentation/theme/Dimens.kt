226a73ec07fabb78995158879256ea4959f5c55b
Marlon D. Rocha <mrocha.solks@digio.com.br>
2024-03-18 18:27:13 -0300

    Changed some common InputText hardcoded dp values to dimens contants values.

--- composeApp/src/commonMain/kotlin/presentation/theme/Dimens.kt
+++ composeApp/src/commonMain/kotlin/presentation/theme/Dimens.kt
@@ -5,8 +5,8 @@ import androidx.compose.ui.unit.sp
 
 val LARGE_PADDING = 24.dp
 val MEDIUM_PADDING = 16.dp
-val SMALL_PADDING = 10.dp
-val EXTRA_SMALL_PADDING = 6.dp
+val SMALL_PADDING = 12.dp
+val EXTRA_SMALL_PADDING = 8.dp
 val SUPER_SMALL_PADDING = 2.dp
 val ZERO_DP = 0.dp
 
@@ -16,3 +16,6 @@ val FONT_14 = 14.sp
 val FONT_16 = 16.sp
 val FONT_32 = 32.sp
 val FONT_48 = 48.sp
+
+val MIN_INPUT_HEIGHT = 48.dp
+val INPUT_HEIGHT = 52.dp
